2639c6478f6beec0066db37ce9f56a210694f841
Allen Guo <guosonglin001@gmail.com>
2024-10-05 17:45:23 +0800

    wip

--- _demos/t0.build.sh
+++ _demos/t0.build.sh
@@ -1,5 +1,27 @@
 ###############################################################################
 
+# check `third_party/llvm/workspace.bzl` for `LLVM_COMMIT`
+ln -s /Users/allen/repos/_compiler/llvm-project-full $PWD/llvm-project-full
+
+DIR=$PWD/llvm-project-full && PPWD=$PWD
+pushd $DIR
+# WORKSPACE
+cat >WORKSPACE <<EOF
+workspace(name = "llvm-raw")
+EOF
+# BUILD.bazel
+cat >BUILD.bazel <<EOF
+EOF
+popd
+
+DIR=$PWD/llvm-project-full && PPWD=$PWD
+pushd $DIR
+# patch
+find $PPWD/third_party/llvm/*.patch -print0 | xargs -0 -I{} git apply {}
+popd
+
+###############################################################################
+
 python configure.py -h
 
 ###############################################################################
@@ -14,13 +36,21 @@ args=(
 )
 python configure.py "${args[@]}"
 
+# cat >>xla_configure.bazelrc <<EOF
+# build --repo_env HERMETIC_PYTHON_VERSION=3.12
+# EOF
+
 ###############################################################################
 
 # https://github.com/bazelbuild/bazelisk
 
-bazelisk aquery "//xla/examples/..."
+bazelisk info
+
+bazelisk query "//xla/..."
 bazelisk query "//xla/examples/..."
 bazelisk query "//xla/service/spmd/shardy/..."
+
+bazelisk aquery "//xla/examples/..."
 
 ###############################################################################
 
